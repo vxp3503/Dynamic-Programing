71796a862c82f03269aa3e0d89c90af177b3fd3e
vxp3503 <vikrantverma725@gmail.com>
2021-04-12 00:41:39 +0530

    Done

--- Subset_sum_problem.cpp
+++ Subset_sum_problem.cpp
@@ -3,24 +3,26 @@ using namespace std;
 bool is_Subset_sum(int Sum,int arr[],int n)
 {
     bool t[n+1][Sum+1];
-    for(int i=0;i<=n;i++)
+    for (int i = 0; i <=n; i++)
     {
-        for(int j=0;j<=Sum;j++)
+        t[i][0]=1;
+    }
+    for (int j = 0; j <=Sum; j++)
+    {
+        t[0][j]=0;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=Sum;j++)
         {
-            if(i==0)
-            {
-                t[i][j]=0;
-            }
-            if(j==0)
-            {
-                t[i][j]=1;
-            }
             if(arr[i-1]<=j)
             {
-                t[i][j]=t[i][j-arr[i-1]]||t[i-1][j];
+                t[i][j]=t[i-1][j-arr[i-1]]||t[i-1][j];
             }
             else
-            t[i][j]=t[i-1][j];
+            {
+                t[i][j]=t[i-1][j];
+            }
         }
     }
     for(int i=0;i<=n;i++)
