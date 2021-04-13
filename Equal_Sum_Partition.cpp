87bb4e1ef85d2a85deffd58d2541cae7fe479d9a
vxp3503 <vikrantverma725@gmail.com>
2021-04-13 22:56:25 +0530

    Done

--- Equal_Sum_Partition.cpp
+++ Equal_Sum_Partition.cpp
@@ -3,11 +3,11 @@ using namespace std;
 bool subset_sum(int arr[],int n,int sum)
 {
     int dp[n+1][sum+1];
-    for(int i=0;i<n;i++)
+    for(int i=0;i<=n;i++)
     {
         dp[i][0]=1;
     }
-    for(int i=1;i<sum;i++)
+    for(int i=1;i<=sum;i++)
     {
         dp[0][sum]=0;
     }
