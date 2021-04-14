8346e553e881dae6aeab495e8ba1580eaff42378
vxp3503 <vikrantverma725@gmail.com>
2021-04-14 23:46:21 +0530

    Done

---
+++ viki.js
@@ -0,0 +1,202 @@
+// console.log("My name is vikrant")
+// let a=window;
+// alert("hello Vikrant")
+// a=prompt("Enter your name")
+// console.log(a);
+
+// a=confirm("yes or no");
+// console.log(a);
+// window.innerHeight
+// window.innerWindth
+
+// a=window.document;
+// console.log(a);
+// scrollX
+// scrollY
+// a=location
+// location.toString()
+// a=window.history;
+
+// var a = document.all;
+// a = document.body
+// a=document.forms
+// a=document.links
+// a=document.getElementById("first")
+// a=a.className
+// a=a.childNodes;
+// a=a.parentNode
+// a.style.color="red";
+// a.innerText="Vikrant"
+// a.innerHTML="<b>Vikrant</b>"
+// a=document.querySelector("#first")
+// a=document.querySelector("div")
+// a.style.color="green"
+// a=document.getElementsByClassName('container')
+// a=document.getElementsByTagName('div')
+// Array.from(a).forEach(element => {
+// });
+// a=document.querySelector('.no')
+// a=document.querySelector('.container')
+// a.children
+// a.nodeType;
+// a.firstElementChild
+// a.childElementCount
+// nextSibling
+// nextElementSibling
+// Node Types:
+// 1. element
+// 2. Attribute
+// 3. Text Node
+// 8. Comment
+// 9. document
+// 10. dottype
+// a=document.createElement('li');
+// a.className= 'childul';
+// a.id='createli'
+// a.setAttribute('tittle','viki')
+// a.innerText='Vikrant'
+// ul=document.querySelector('ul.this')
+// ul.appendChild(a)
+// elem=document.createElement('h3');
+// elem.id='elem2'
+// elem.className='elem2';
+// text=document.createTextNode("my name is viki")
+// elem.appendChild(text);
+// a.replaceWith(elem)
+// getAttribute
+// hasAttribute
+//removeAttribute
+// a=document.createElement('li');
+// a.className= 'childul';
+// a.id='createli'
+// a.setAttribute('tittle','viki')
+// a.innerText='Vikrant'
+//     elem=document.querySelector('.this')
+//     elem.replaceChild(a,document.querySelectorAll('.childul')[0])
+//     console.log(elem);
+// console.log("You have click the heading")
+// document.getElementById("heading").addEventListener('mouseover',function(j){
+// console.log(j)
+// location.href="facebook.com"
+// v=j.target
+// v=j.target.classList
+// })
+// console.log("Vikrant")
+// }
+// btn=document.getElementById('btn')
+// btn.addEventListener('click',function(e)
+// {
+//     console.log('thanks',e)
+//     e.preventDefault()
+// })
+// btn = document.getElementById('btn')
+// // btn.addEventListener('click',function(e)
+// {
+// console.log(e);
+// e.preventDefault();
+// })
+// mouseenter,mouseleave,mousemove
+// document.querySelector('.container').addEventListener('mousemove',function(e)
+// {
+//     document.body.style.backgroundColor=`rgb(${e.offsetX},${e.offsetY},${e.offsetY})`
+// })
+// localStorage.setItem('Name','Vikrant')
+// n=localStorage.getItem('Name')
+// console.log(n)
+// localStorage.clear()
+// localStorage.removeItem('Name')
+// JSON.parse() //convert string to array
+// JSON.stringify() 
+// convert array to string
+// sessionStorage.setItem('Name','Vikrant')
+
+// const arr=[]
+// for(var i=0;i<5;i++)
+// {
+// arr.push((function(i){
+// return function() {
+// console.log(i)
+// }
+//  })(i))
+// }
+// arr[0]()
+// window.onload= js;
+
+//     let obj= {
+//         name: "Verna",
+//         topspeed: 300,
+//         run: function car() {
+//             console.log("Car is running")
+
+//         }
+//     }
+//     console.log(obj)
+//   function gencar(name ,topspeed) {
+//       this.name=name
+//       this.topspeed=
+//       this.run=function() {
+//           console.log(`${this.name} is running`)
+
+//       }
+//   }
+//   car1 =new gencar('Nissan',180);
+//   console.log(car1)
+
+
+
+// let obj={
+//     name: "vikrant",
+//     Address: "Tripal"
+// }
+
+// class Employee{
+//     constructor(name,experience,division)
+//     {
+//         this.name=name
+//         this.experience=experience
+//         this.division=division
+//     }
+//     experiencefrom2000(a)
+//     {
+//         return 2019-a;
+//     }
+// }
+// class Programmer extends Employee{
+//     constructor(name,experience,language,github)
+//     {
+//         super(name,experience)
+//         this.language=language
+//         this.github=github
+//     }
+//     static add(a,b)
+//     {
+//         return a+b;
+//     }
+// }
+// viki=new Employee("vikrant",14,"B.tech")
+// console.log(viki)
+// console.log(viki.experiencefrom2000(5))
+// tiki=new Programmer("vikrant",5,"C++","vxp3503")
+// console.log(tiki)
+
+// let v=0;
+// for(let i=0;i<1;i=0)
+// {
+//     console.log(v)
+//     v=v+1;
+// }
+// viki = document.createElement('div')
+// div = document.querySelectorAll(".no")
+// viki.innerText = "Vikrant"
+// div[3].appendChild(viki)
+// let v = 0;
+// document.querySelector('.container').addEventListener('mousemove', function (e) {
+//     viki.innerText = `${e.offsetX} ${e.offsetY} ${e.offsetY}`
+    
+//     document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${e.offsetY})`
+// })
+
+window.onload = js;
+
+function js() {
+}
