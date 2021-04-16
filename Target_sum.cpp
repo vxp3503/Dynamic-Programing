1ae2a585b4066f7e6c81da550654b7b5f00285ae
vxp3503 <vikrantverma725@gmail.com>
2021-04-16 23:43:34 +0530

    Done

---
+++ Target_sum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Target_sum(int arr[],int n)
+{
+    
+}
+int main()
+{
+    cout<<"Enter the no. of elements";
+    int n;
+    cin>>n;
+    cout<<"Enter the elements";
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<Target_sum(arr,n);
+}
