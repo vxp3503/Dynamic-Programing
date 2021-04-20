81d611bcb58dc9fff33f77dac462642230e7835c
vxp3503 <vikrantverma725@gmail.com>
2021-04-20 23:02:24 +0530

    Done

---
+++ Unbounded-Knapsack.cpp/Coin_Change_Poblem_maximum_no._of_ways.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int coin_change_problem_minimum_number_of_ways(int coins[],int n,int amount)
+{
+        int dp[n+1][amount+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+       for(int i=1;i<=amount;i++)
+        {
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+                }
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][amount];
+}
+int main()
+{
+    int n;
+    cout<<"Enter the no. of coins";
+    cin>>n;
+    int coins[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>coins[i];
+    }
+    cout<<"Enter the amount";
+    int amount;
+    cin>>amount;
+    cout<<coin_change_problem_minimum_number_of_ways(coins,n,amount);
+}
